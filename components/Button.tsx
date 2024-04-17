021176b706949ab77fbff02cf45977c386d089e3
praise-cloud <georgewillpraise15@gmail.com>
2024-04-17 17:09:43 +0100

    first full stack project

---
+++ components/Button.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+// for it to know the type of props it should get
+type ButtonProps = {
+    type: 'button' | 'submit';
+    title : string;
+    icon?: string; // the ? is for the optional icon
+    variant: string;
+}
+
+const Button = ({type, title, icon, variant}: ButtonProps) => {
+  return (
+    <button type={type} className={`flexCenter gap-3 rounded-full border ${variant}`}>
+        {icon &&  <Image src = {icon} alt = {title} width={24} height={24}/>}
+        <label className="bold-16 whitespace-normal">{title}</label>
+    </button>
+  )
+}
+
+export default Button
